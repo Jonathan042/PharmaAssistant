941a39a7792f5abfde9adff93bb1a8050e055640
jyo4 <jyo4@cornell.edu>
2021-07-30 15:44:41 -0400

    Project made

---
+++ PharmaAssistant/src/main/java/cogent/PharmaAssistantApplication.java
@@ -0,0 +1,13 @@
+package cogent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmaAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmaAssistantApplication.class, args);
+	}
+
+}
---
+++ PharmaAssistant/src/test/java/cogent/PharmaAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package cogent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmaAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
