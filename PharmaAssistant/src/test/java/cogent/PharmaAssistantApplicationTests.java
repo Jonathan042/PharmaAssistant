f16c8f4f5a0eb6516b0a59386d667ae5a4fe3d21
patsums <kahsumspat90@gmail.com>
2021-08-10 17:01:18 -0400

    Added Junit test with mockito

--- PharmaAssistant/src/test/java/cogent/PharmaAssistantApplicationTests.java
+++ PharmaAssistant/src/test/java/cogent/PharmaAssistantApplicationTests.java
@@ -1,13 +1,42 @@
 package cogent;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import cogent.dao.UserRepository;
+import cogent.entities.User;
+import cogent.service.UserDetailsServiceImpl;
+
+
 
 @SpringBootTest
 class PharmaAssistantApplicationTests {
+	
+	@MockBean
+	private UserRepository repository;
+	
+	@Autowired
+	private UserDetailsServiceImpl usds;
 
 	@Test
-	void contextLoads() {
+	public void loadUserByUsernameTest() {
+		
+		User user = new User("patsums","123");
+		Optional<User> us = Optional.of(user);
+		when(repository.findByUsername("patsums")).thenReturn(us);
+		//assertEquals(user,usds.loadUserByUsername("patsums"));
+		assertEquals(user.getUsername(),usds.loadUserByUsername("patsums").getUsername());
+		assertEquals(user.getPassword(),usds.loadUserByUsername("patsums").getPassword());
+		
 	}
+	
+	
 
 }
